c718e8d6f2e3d7faad898a2195c82e13f8599eb5
Manuela Hernadez <manu.hdz.0795@gmail.com>
2024-02-23 02:00:08 -0500

    Se agrega buscador por titulo

--- challenge-comfama/src/components/CardAnime.jsx
+++ challenge-comfama/src/components/CardAnime.jsx
@@ -1,8 +1,6 @@
-
-
 export default function CardAnime({title, image, score}) {
     return (
-      <section className="bg-sky-900">
+      <section className="bg-cyan-700 rounded-md my-4">
             <div className='my-4'>
                 <img src={image} alt={title} className='w-full h-80 object-cover rounded-lg' />
                   <h3 className='mt-2 text-center font-semibold text-slate-200'>{title}</h3>
--- challenge-comfama/src/pages/Dashboard.jsx
+++ challenge-comfama/src/pages/Dashboard.jsx
@@ -1,31 +1,65 @@
 import { getAnimes } from '../services/request';
 import { useEffect, useState } from 'react';
-import CardAnime from '../components/CardAnime'
+import CardAnime from '../components/CardAnime';
 
 export default function Dashboad() {
   const [animeList, setAnimes] = useState([]);
+  const [animeListFiltered, setAnimesFiltered] = useState([]);
+  const [searchTitle, setSearchTitle] = useState('');
+
+
+
 
   useEffect(() => {
     getAnimes()
       .then((response) => {
         console.log(response.data.data);
         setAnimes(response.data.data);
+        setAnimesFiltered(response.data.data);
       })
       .catch((error) => {
         console.log((error));
       });
   }, []);
 
+
+  useEffect(() => {
+    const filteredResults = animeList.filter(item =>
+      item.title.toLowerCase().includes(searchTitle.toLowerCase())
+    );
+    setAnimesFiltered(filteredResults);
+  }, [searchTitle]);
+
+
+  
+
+  const handleChange = event => {
+    setSearchTitle(event.target.value);
+  };
+
   return (
-    <div className='bg-slate-900'>
-      <div className='grid grid-cols-1 mx-2 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 gap-8'>
-        {
-          animeList.map((anime) => (
-            <CardAnime title={anime.title} image={anime.images.jpg.image_url} key={anime.mal_id} score={anime.score} />
-          ))
-        }
-      </div>
+    <section className='bg-sky-900'>
+      <div className='flex justify-center'>
+      <input
+        type="text"
+        placeholder="Search by title"
+        value={searchTitle}
+        onChange={handleChange}
+        className="my-6 w-1/4 rounded-md text-xl"
+      />
     </div>
+      <div >
+        <div className='grid grid-cols-1 mx-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+          {
+            animeListFiltered.map((anime) => (
+              <CardAnime title={anime.title} image={anime.images.jpg.image_url} key={anime.mal_id} score={anime.score} />
+            ))
+          }
+        </div>
+      </div>
+
+    </section>
+
 
 
   )
